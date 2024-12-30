437c35f02ee23e828a258c882ab645ac515fe452
Kassim Zaid <81573513+kzaid0767@users.noreply.github.com>
2024-12-30 12:48:37 -0500

    learned CRUD in MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
 
 
 const app = express();
@@ -24,6 +24,25 @@ const connectToDatabase = async () => {
     try {
         await client.connect();
         console.log("Connected to MongoDB");
+        /*
+        1.database name
+        2.collection name
+        3. students data 
+         */
+        const database = client.db("all-students");
+        const collection = database.collection("students");
+        
+        //const result = await collection.find({role: "student"}).limit(2).toArray();
+        //update id 6772c8425bb486bb78f3ed87
+        //const result = await collection.updateOne({_id: new ObjectId("6772c8425bb486bb78f3ed87")}, {$set: {age: 43}});
+        //delete
+        // const result = await collection.findOneAndUpdate({name: "Sharifa Zaid"}, {$set: {age: 11}});
+        // const result =  await collection.findOne({name: "Sharifa Zaid"});
+        // const result = await collection.updateMany({role: "student"}, {$set: {school: "KZS"}});
+        // const result = await collection.insertOne({_id: 67, role: "student", name: "Shafa Zaid", age: 12, school: "KZS"});
+        const result = await collection.deleteOne({_id: 67});
+        console.log(result);
+
     } catch (error) {
         console.log(error);
     }
