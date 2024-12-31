4d5a1f28bd0fac996b9b309a72511e2c055eac3d
Kassim Zaid <81573513+kzaid0767@users.noreply.github.com>
2024-12-31 07:04:12 -0500

    on to solo project

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const connectToDatabase = async () => {
         3. students data 
          */
         const database = client.db("all-students");
-        const collection = database.collection("students");
+        const collection = database.collection("teachers");
         
         //const result = await collection.find({role: "student"}).limit(2).toArray();
         //update id 6772c8425bb486bb78f3ed87
@@ -40,7 +40,24 @@ const connectToDatabase = async () => {
         // const result =  await collection.findOne({name: "Sharifa Zaid"});
         // const result = await collection.updateMany({role: "student"}, {$set: {school: "KZS"}});
         // const result = await collection.insertOne({_id: 67, role: "student", name: "Shafa Zaid", age: 12, school: "KZS"});
-        const result = await collection.deleteOne({_id: 67});
+        // const result = await collection.deleteOne({_id: 67});
+        // const result = await collection.deleteMany({role: "teacher"});
+        // const result = await collection.findOneAndDelete({role: "sudent"});
+        /* const result = await collection.insertMany([
+            {_id: 21, subject:'math', role: "teacher", name: "Jafar Jaid", age: 22, school: "KZS"},
+            {_id: 22, subject: 'science', role: "teacher", name: "Safa Kaid", age: 32, school: "KZS"},
+            {_id: 23, subject: 'english', role: "teacher", name: "Tahira Paid", age: 32, school: "KZS"},
+            {_id: 24, subject: 'arabic', role: "teacher", name: "Jamil Said", age: 52, school: "KZS"},
+            
+        ]) */
+
+        // const result = await collection.find({subject: {$eq: "math"}, age: {$lt: 30}}).toArray();
+        //with logical operators
+        // const result = await collection.find({$and: [{role: "teacher"}, {age: {$gte: 30}}]}).toArray();
+
+        // const result = await collection.find({$or: [{role: "teacher"}, {age: {$gt: 30}}]}).toArray();
+
+        const result = await collection.find({$nor: [{subject: "math"}, {age: {$eq: 30}}]}).toArray();
         console.log(result);
 
     } catch (error) {
